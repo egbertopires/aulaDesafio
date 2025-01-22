7a6901366f1a37458cb7cc84e12978b150aa5071
egberto pires <egbertojap@gmail.com>
2025-01-21 21:45:46 -0300

    Adiciona loop para repetir verificação de idade

--- idade.py
+++ idade.py
@@ -1,8 +1,15 @@
-idade = int(input('Informe sua idade: '))
+while True:
+    idade = int(input('Informe sua idade: '))
 
-if idade >= 18:
-    retorno = 'Maior de idade'
-else:
-    retorno = 'Menor de idade'
+    if idade >= 18:
+        retorno = 'Maior de idade'
+    else:
+        retorno = 'Menor de idade'
 
-print(retorno)
+    print(retorno)
+
+    continuar = input('Deseja continuar? [S/N]: ').strip().upper()
+
+    if continuar in ['NÃO','NAO','N']:
+        print('Programa encerrado...')
+        break
